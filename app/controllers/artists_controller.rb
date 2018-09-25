bbbf1420b4c54910b8dd42772a97f288e6745964
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-25 21:09:01 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   before_action :set_preferences, only: [:index, :show]
+  
   def index
     @artists = Artist.all
   end
@@ -9,7 +10,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if @preferences && !@preferences.allow_create_artists
+      redirect_to artists_path
+    else
+      @artist = Artist.new
+    end
   end
 
   def create
