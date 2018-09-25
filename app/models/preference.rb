2b5f90a2da303fdd8dab96caf941f055efa5ec00
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-25 20:32:26 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,15 +1,3 @@
 class Preference < ActiveRecord::Base
-
-  def allow_create_songs
-  end
-
-  def allow_create_songs=(preference)
-  end
-
-  def allow_create_artists
-  end
-
-  def allow_create_artists=(preference)
-
-  end
+  
 end
